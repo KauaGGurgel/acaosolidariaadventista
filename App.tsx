e6ca1867cf5e6dccae4532e6633be327d45a7dd8
KauaGGurgel <kauaggurgel@gmail.com>
2026-02-10 20:06:24 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 import { createClient } from "@supabase/supabase-js";
 import {
   LayoutDashboard,
